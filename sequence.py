225a9b068f98b2aed9676fe1bcb6d17764fc78a5
fernan-x <fabien.fernandes-alves@epitech.eu>
2019-03-14 10:48:49 +0300

    Add files via upload

--- sequence.py
+++ sequence.py
@@ -2,7 +2,7 @@
 
 import argparse
 
-def traceback(scoringMatrix, seqA, seqB, seqAsize, seqBsize):
+def traceback(scoringMatrix, seqA, seqB, seqAsize, seqBsize, match, missmatch, gap):
     AlignmentA = ""
     AlignmentB = ""
     i = seqAsize - 1
@@ -11,34 +11,34 @@ def traceback(scoringMatrix, seqA, seqB, seqAsize, seqBsize):
         score = scoringMatrix[i][j]
         valueScoreDiag = 0
         if seqA[i - 1] == seqB[j - 1]:
-            valueScoreDiag = 4
+            valueScoreDiag = match
         else:
-            valueScoreDiag = -3
+            valueScoreDiag = missmatch
         scoreDiag = scoringMatrix[i - 1][j - 1]
         scoreUp = scoringMatrix[i - 1][j]
         scoreLeft = scoringMatrix[i][j - 1]
-        print("Score : " + str(score) + ", Score Diag : " + str(scoreDiag) + ", Score Up :  " + str(scoreUp) + ", Score Left : " + str(scoreLeft))
+        #print("Score : " + str(score) + ", Score Diag : " + str(scoreDiag) + ", Score Up :  " + str(scoreUp) + ", Score Left : " + str(scoreLeft))
         if (score == scoreDiag + valueScoreDiag):
-            print("Go diag")
+            #print("Go diag")
             AlignmentA = seqA[i - 1] + AlignmentA
             AlignmentB = seqB[j - 1] + AlignmentB
-            print(AlignmentA)
-            print(AlignmentB + "\n")
+            #print(AlignmentA)
+            #print(AlignmentB + "\n")
             i -= 1
             j -= 1
-        elif (score == scoreLeft - 2):
-            print("Go left")
+        elif (score == scoreLeft + gap):
+            #print("Go left")
             AlignmentA = "-" + AlignmentA
             AlignmentB = seqB[j - 1] + AlignmentB
-            print(AlignmentA)
-            print(AlignmentB + "\n")
+            #print(AlignmentA)
+            #print(AlignmentB + "\n")
             j -= 1
         else:
-            print("Go up")
+            #print("Go up")
             AlignmentA = seqA[i - 1] + AlignmentA
             AlignmentB = "-" + AlignmentB
-            print(AlignmentA)
-            print(AlignmentB + "\n")
+            #print(AlignmentA)
+            #print(AlignmentB + "\n")
             i -= 1
 
     while i > 0:
@@ -53,22 +53,23 @@ def traceback(scoringMatrix, seqA, seqB, seqAsize, seqBsize):
 
     print(scoringMatrix)
     print("\n")
-    print(AlignmentA)
-    print(AlignmentB)
+    print("Alignment A: " + AlignmentA)
+    print("Alignment B: " + AlignmentB)
+    print("Score = " + str(scoringMatrix[seqAsize - 1][seqBsize - 1]))
 
-def fillMatrix(scoringMatrix, seqA, seqB, seqAsize, seqBsize):
+def fillMatrix(scoringMatrix, seqA, seqB, seqAsize, seqBsize, match, missmatch, gap):
     i = 1
     while i < seqAsize:
         j = 1
         while j < seqBsize:
             scroeDiag = 0;
             if (seqA[i - 1] == seqB[j - 1]):
-                scroeDiag = scoringMatrix[i - 1][j - 1] + 4
+                scroeDiag = scoringMatrix[i - 1][j - 1] + match
             else:
-                scroeDiag = scoringMatrix[i - 1][j - 1] + -3
+                scroeDiag = scoringMatrix[i - 1][j - 1] + missmatch
 
-            scroeLeft = scoringMatrix[i][j - 1] - 2
-            scroeUp = scoringMatrix[i - 1][j] - 2
+            scroeLeft = scoringMatrix[i][j - 1] + gap
+            scroeUp = scoringMatrix[i - 1][j] + gap
 
             maxScore = max([max([scroeDiag, scroeLeft]), scroeUp]);
 
@@ -76,9 +77,9 @@ def fillMatrix(scoringMatrix, seqA, seqB, seqAsize, seqBsize):
             j += 1
         i += 1
 
-    traceback(scoringMatrix, seqA, seqB, seqAsize, seqBsize)
+    traceback(scoringMatrix, seqA, seqB, seqAsize, seqBsize, match, missmatch, gap)
 
-def init(seqA, seqB):
+def init(seqA, seqB, match, missmatch, gap):
     print("Sequence A : " + seqA)
     print("Sequence B : " + seqB)
     seqAsize = len(seqA) + 1
@@ -88,18 +89,21 @@ def init(seqA, seqB):
     #Initialization Step
     i = 1
     while i < seqAsize:
-        scoringMatrix[i][0] = i * -2
+        scoringMatrix[i][0] = i * gap
         i += 1
 
     j = 1
     while j < seqBsize:
-        scoringMatrix[0][j] = j * -2
+        scoringMatrix[0][j] = j * gap
         j += 1
 
-    fillMatrix(scoringMatrix, seqA, seqB, seqAsize, seqBsize)
+    fillMatrix(scoringMatrix, seqA, seqB, seqAsize, seqBsize, match, missmatch, gap)
 
 parser = argparse.ArgumentParser(description='Performs the sequence alignment on 2 sequences')
 parser.add_argument('seqA', type=str, help='Sequence A')
 parser.add_argument('seqB', type=str, help='Sequence B')
+parser.add_argument('match', type=int, help='Match value')
+parser.add_argument('missmatch', type=int, help='MissMatch value')
+parser.add_argument('gap', type=int, help='Gap value')
 args = parser.parse_args()
-init(args.seqA, args.seqB)
+init(args.seqA, args.seqB, args.match, args.missmatch, args.gap)
